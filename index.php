6c36e764946b7cca0d0d4534534a7308cb0085e1
Jonás Damián <jonas.damher@gmail.com>
2021-03-06 09:15:33 +0000

    update index for new environment

--- index.php
+++ index.php
@@ -2,25 +2,19 @@
 
 declare(strict_types=1);
 
-error_reporting(E_ALL);
-
 session_start();
 
 require_once 'libs/Environment.php';
 Environment::init();
-
 require_once 'config/Globals.php';
+error_reporting(ERROR_REPORTING);
 
 require_once 'helpers/Utils.php';
-
 require_once 'libs/Auth.php';
- 
 require_once 'core/DatabaseHandler.php';
 require_once 'core/Database.php';
-
 require_once 'libs/ResponseHandler.php';
 require_once 'libs/Validator.php';
 require_once 'core/BaseModel.php';
 require_once 'core/BaseController.php';
-
 require_once 'libs/Router.php';
